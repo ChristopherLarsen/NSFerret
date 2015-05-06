fc8aa74341f303baa0a2e55ad531c87a91f7cede
Christopher Larsen <christopher@deadratgames.com>
2015-05-05 23:59:32 -0400

    First Ferret

---
+++ NSFerret.h
@@ -0,0 +1,13 @@
+//
+//  NSFerret.h
+//  DeadRatGames
+//
+//  Created by Christopher Larsen on 2013-07-13.
+//  Copyright (c) 2013 DeadRatGames All rights reserved.
+
+
+@interface NSFerret : UIView <UIGestureRecognizerDelegate>
+
++ (NSFerret *)ferret;
+
+@end
